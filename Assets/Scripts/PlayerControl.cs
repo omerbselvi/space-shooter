93adfcc7fb29a578d0486a3ed374a2bd9ca82e6f
Ömer Buğra Selvi <omerbselvi@omerbselvi-2.local>
2018-10-02 02:55:35 +0300

    Add playercontroller script

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerControl : MonoBehaviour {
+
+    private Rigidbody playerRb;
+    public float speed;
+    public float tiltMultiplier;
+    public Boundary boundary;
+
+    private void Start() {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        playerRb.velocity = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+        playerRb.position = new Vector3(
+            Mathf.Clamp(playerRb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(playerRb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        playerRb.rotation = Quaternion.Euler(0.0f, 0.0f, -playerRb.velocity.x * tiltMultiplier);
+    }
+}
