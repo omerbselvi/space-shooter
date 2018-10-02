1e321b8e57fcb9cdc2ed90079666c3b40c1c3909
omerbselvi <omerbugra97@hotmail.com>
2018-10-03 01:44:26 +0300

    Add Explosion effect when collision happens

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other) {
         if(other.tag != "Boundary"){
+            Instantiate(explosion, transform.position, transform.rotation);
+            if(other.tag == "Player"){
+                Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            }
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
---
+++ Assets/Scripts/MoveAsteroid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAsteroid : MonoBehaviour {
+
+    Rigidbody rbAsteroid;
+    public float speed;
+
+    private void Start() {
+        rbAsteroid = GetComponent<Rigidbody>();
+        rbAsteroid.velocity = transform.forward * speed;
+    }
+}
