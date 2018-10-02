16b5d752124a5b962d2cdb0608812047f193fc3e
omerbselvi <omerbugra97@hotmail.com>
2018-10-02 21:18:54 +0300

    Add Bolt script and Bolt prefab

---
+++ Assets/Scripts/Bolt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolt : MonoBehaviour {
+
+    Rigidbody rbBolt;
+    public float speed;
+
+    private void Start() {
+        rbBolt = GetComponent<Rigidbody>();
+        rbBolt.velocity = transform.forward * speed;
+    }
+}
