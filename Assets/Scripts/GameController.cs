fe7b43304e4791ee0316fe54cc6300bbaa14e131
omerbselvi <omerbugra97@hotmail.com>
2018-10-03 21:46:18 +0300

    Add waves of asteroids and some memory optimization

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Vector3 positionAsteroid;
+    public GameObject asteroid;
+    public int hazardCount;
+    public float startWait;
+    public float spawnWait;
+    public float waitForWaves;
+
+    private void Start() {
+        StartCoroutine(spawnWaves());
+    }
+
+    private IEnumerator spawnWaves(){
+        yield return new WaitForSeconds(startWait);
+        while(true){
+            for (int i = 0; i < hazardCount;i++){
+                Vector3 position = new Vector3(Random.Range(-positionAsteroid.x, positionAsteroid.x), positionAsteroid.y, positionAsteroid.z);
+                Quaternion rotation = Quaternion.identity;
+                Instantiate(asteroid, position, rotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waitForWaves);
+        }
+    }
+
+}
