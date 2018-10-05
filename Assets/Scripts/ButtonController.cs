007252a9cbe5f57b9bb3c7bce4f7906fbea8cad4
omerbselvi <omerbugra97@hotmail.com>
2018-10-06 02:11:17 +0300

    Add a simple Menu scene

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+	public void startGame() {
+        Debug.Log("Hello");
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+
+    }
+
+}
